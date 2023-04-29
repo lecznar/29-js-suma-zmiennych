93ca825b0962650e4e78bbe8367e4eab01ebd401
llecznar <lecznar@gmx.de>
2023-04-29 19:04:33 +0200

    zadanie

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let firstNumber = 2;
+let secondNumber = 7;
+
+function sum (firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+  }
+
+  console.log( sum(firstNumber, secondNumber) );
